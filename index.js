d648c18944f070bcfc79f91362cd4b0d1b92f6a4
pandey2app <pandey2app@gmail.com>
2024-08-16 18:40:50 +0530

    added view

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const app = express();
 const port = 4000;
 const names = ['app', 'aditya']
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.get('/names', (req, res) => {
   res.json({names});
+});
+
+app.get('/ejs', (req, res) => {
+  res.render("index");
 });
 app.listen(port, (err, res) => {
     if (err) {
